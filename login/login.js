d4b5ef95c0ab22b8c0d6440625e00b1ba5f9860d
udhai-20 <udhayaroger511@gmail.com>
2022-07-23 23:07:47 +0530

    added

--- login/login.js
+++ login/login.js
@@ -1,4 +1,6 @@
 document.querySelector("form").addEventListener("submit", login);
+// let loginuser = JSON.parse(localStorage.getItem("login")) || [];
+let userlogindetails = JSON.parse(localStorage.getItem("useremail")) || [];
 function login(e) {
   console.log(e);
   e.preventDefault();
@@ -12,17 +14,26 @@ function login(e) {
     alert("please fill input field");
   } else {
     let flag = "false";
-    let userlogindetails = JSON.parse(localStorage.getItem("useremail")) || [];
+
     userlogindetails.forEach(function (e) {
       if (obj.loginemail == e.email && obj.loginpassword == e.pass) {
         flag = "true";
+        // loginuser.push(e);
+        alert("logged in sucessful");
+        localStorage.setItem("login", JSON.stringify(e));
+        window.location.href = "index.html";
       }
     });
-    if (flag === "true") {
-      alert("logged in sucessful");
-      window.location.href = "navbar.html";
-    } else if (flag === "false") {
+    if (flag === "false") {
       alert("Please check the entered email or password incorrect");
     }
   }
+}
+// username
+let loginuser = JSON.parse(localStorage.getItem("login"));
+
+if (loginuser != null) {
+  let name = document.getElementById("username");
+  name.style.color = "blue";
+  name.innerText = loginuser.name1;
 }
